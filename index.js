13bbfc5fc12c93842c1bf2fd23770db960ebfe4c
ryoohshima <ryo.ohshima@actdesign-lab.co.jp>
2020-04-16 10:03:39 +0900

    update

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-$(document).on("ckick", function() {
+// opening
+$(document).ready(function() {
+    // 文字を順番に表示
+    var arrey = [
+        "hello",
+        "name",
+        "job",
+        "title"
+    ]
+    var i = 0;
+    var opening = function() {
+        if($(".opening-" + arrey[i]).hide()) {
+            $(".opening-" + arrey[i]).show();
+            i ++;
+        }
+        if(i > arrey.length) {
+            i = "";
+            $(".opening").hide();
+            $(".home").show();
+            clearInterval(opening);
+        }
+    }
+    setInterval(opening, 1000);
 
-}
+
+});
